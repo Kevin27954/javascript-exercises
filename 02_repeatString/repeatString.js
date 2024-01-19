7deba9fb6d9245041f03d36da9b61a23aa8a977b
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2024-01-19 00:46:54 -0500

    Work on js exercises 01-04 all passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats) {
+    if(repeats < 0) {
+        return 'ERROR';
+    }
+    let newWord = '';
+    for(let i = 0; i < repeats; i++) {
+        newWord = newWord.concat(word);
+    }
+    return newWord;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputs, ...args) {
+    let arr = []
+    arr = inputs.filter((input) => {
+        for(let i = 0; i < args.length; i++) {
+            if(input === args[i]) {
+                return false;
+            }
+        }
+        return true;
+    })
+    return arr;
 };
 
 // Do not edit below this line
