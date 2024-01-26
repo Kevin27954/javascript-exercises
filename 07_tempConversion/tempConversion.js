7169072e02e231641a8cecdf68eba600310c60e2
Kevin27954 <98406775+Kevin27954@users.noreply.github.com>
2024-01-26 09:07:35 -0500

    Finished exercies

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  // (32°F − 32) × 5/9 = 0°C
+  return Number.parseInt(((tempF - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return Number.parseInt(((5/9) * tempC + 32).toFixed(1));
 };
 
 // Do not edit below this line
